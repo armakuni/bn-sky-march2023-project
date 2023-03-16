91dd5adc4d8a343877eeaa0c23b6440b479a9cfc
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-03-16 19:45:40 +0000

    add products route to API

--- app/db-api/src/routeHandlers.py
+++ app/db-api/src/routeHandlers.py
@@ -12,3 +12,25 @@ class UserRoutes:
                 return [
                     {"id": int(id), "username": username} for (id, username) in cursor
                 ]
+
+
+class ProductRoutes:
+    def __init__(self, db):
+        self.db = db
+
+    def products(self):
+        with closing(self.db.readConnection()) as cnx:
+            with closing(cnx.cursor()) as cursor:
+                cursor.execute(
+                    "SELECT id, name, price, quantity, imageurl FROM products"
+                )
+                return [
+                    {
+                        "id": int(id),
+                        "name": name,
+                        "price": float(price),
+                        "quantity": quantity,
+                        "imageurl": imageurl,
+                    }
+                    for (id, name, price, quantity, imageurl) in cursor
+                ]
---
+++ app/db-api/tests/test_ProductRoutes.py
@@ -0,0 +1,38 @@
+from .fakeDatabase import FakeDB
+from src.routeHandlers import ProductRoutes
+
+
+def test_products():
+    # arrange
+    db = FakeDB(
+        [
+            [1, "Psychadelic snorkling kit", 49.99, 5, None],
+            [
+                2,
+                "Pirate tennis gear",
+                19.99,
+                10,
+                "http://sillyimages.com/pirate-tennis.png",
+            ],
+        ]
+    )
+    productRoutes = ProductRoutes(db)
+    # act
+    result = productRoutes.products()
+    # assertion
+    assert result == [
+        {
+            "id": 1,
+            "name": "Psychadelic snorkling kit",
+            "price": 49.99,
+            "quantity": 5,
+            "imageurl": None,
+        },
+        {
+            "id": 2,
+            "name": "Pirate tennis gear",
+            "price": 19.99,
+            "quantity": 10,
+            "imageurl": "http://sillyimages.com/pirate-tennis.png",
+        },
+    ]
