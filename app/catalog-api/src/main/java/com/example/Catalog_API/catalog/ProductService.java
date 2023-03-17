57069ba3ca53b0604cfa72f1b19742b2cc758036
saragilani <s.g.ahmed163@gmail.com>
2023-03-17 11:24:37 +0000

    added post, get, delete methods to ProductService

--- app/catalog-api/src/main/java/com/example/Catalog_API/catalog/ProductService.java
+++ app/catalog-api/src/main/java/com/example/Catalog_API/catalog/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.Catalog_API.catalog;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,28 +9,97 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    private String db_api_url = "";
+    
     @Autowired
     ProductRepository productRepository;
 
     // Adding new product
     public Product addNewproduct(Product product){
-        productRepository.save(product);
-        return product;
+        // productRepository.save(product);
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + db_api_url + "/products"))
+                    .method("POST"
+                    , product)
+                    .build();
+            HttpResponse<String> response = null;
+
+            Product responseProduct = null;
+            
+            try {
+                response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+                responseProduct = new Product(response.body().name, response.body().price, response.body().quantity,)
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        
+    
+        return ;
     }
 
-    //Deleting product
-    public void deleteProduct(Long id){
-    productRepository.deleteById(id);
+    // Deleting product
+    public void deleteProduct(int id){
+            // productRepository.deleteById(id);
+            HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://" + db_api_url + "/products?id=" + String.valueOf(id)))
+            .method("DELETE"
+            , null)
+            .build();
+        HttpResponse<String> response = null;
+
+        try {
+        response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+        e.printStackTrace();
+        } catch (InterruptedException e) {
+        e.printStackTrace();
+        }
+        return null;
     }
 
     //Get all products
     public List<Product>getAllProducts (){
-        return productRepository.findAll();
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + db_api_url + "/products"))
+                    .method("GET"
+                    , null)
+                    .build();
+            HttpResponse<String> response = null;
+            
+            try {
+                response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        return response.body();
     }
 
     //Getting product by Id
-    public Optional<Product> getProductById (Long id){
-        return productRepository.findById(id);
+    public Optional<Product> getProductById (int id){
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + db_api_url + "/products?id=" + String.valueOf(id)))
+                    .method("GET"
+                    , null)
+                    .build();
+            HttpResponse<String> response = null;
+            
+            try {
+                response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        return response.body();
     }
 
-    }
+}
+
+
+   
